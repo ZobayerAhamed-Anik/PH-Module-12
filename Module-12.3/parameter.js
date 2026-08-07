d8694ce2499b5527dc67ce13193432d9e6f9093b
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-07 22:23:18 +0600

    Parameterized function Example

--- Module-12.3/parameter.js
+++ Module-12.3/parameter.js
@@ -10,11 +10,18 @@ sayHello('Shaku');
 if(4 < 5){
     sayHello('Ruban');
 }
+console.log();
 
 //2.
 function add(number1, number2){
     console.log('You have provided', number1, number2);
-    console.log(number1, ' + ', number2, ' = ',number1 + number2);
+    console.log(number1, ' + ', number2, ' = ', number1 + number2);
 }
 
 add(234, 678);
+
+function sumOfAll(a, b, c, d){
+    console.log('Sum of 4 numbers', a + b + c + d);
+}
+
+sumOfAll(123, 432, 323, 447); 
