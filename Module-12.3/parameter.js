d651abd1d205f8119c9bdfcdf45fe90c35578d6e
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-07 22:15:22 +0600

    Module-12: Functions in JS

---
+++ Module-12.3/parameter.js
@@ -0,0 +1,20 @@
+//parameter
+
+function sayHello(name){
+    console.log("Hello! to bhai,", name);
+}
+
+sayHello('Sallu');
+sayHello('Shaku');
+
+if(4 < 5){
+    sayHello('Ruban');
+}
+
+//2.
+function add(number1, number2){
+    console.log('You have provided', number1, number2);
+    console.log(number1, ' + ', number2, ' = ',number1 + number2);
+}
+
+add(234, 678);
