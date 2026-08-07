b7b4009d738fd3f0b8d4df55d2c59217ebfc2bc6
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-07 22:54:46 +0600

    Function return and arguments

---
+++ Module-12.4/arguments.js
@@ -0,0 +1,15 @@
+function multiply(a, b){// parameters
+    const result = a * b;
+    console.log('Result of multiplication is:', result);
+}
+multiply(4, 5, 34, 48, 50); // arguments
+
+function square(number){
+    const result = number * number;
+    console.log('Square of the number provided', result);
+}
+// console.log('Number ase naki vai', number);
+square(7);
+console.log('---------------------------------');
+const money = 25;
+square(money);
